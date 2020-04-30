aaf1972f912cc5ba4d95a2fdcea7fdfeac3e80c6
Jose Miguel <jmnr.1311993@gmail.com>
2020-04-30 12:48:04 -0500

    Inicializando repositorio

---
+++ src/main/java/biz/belcorp/spring/springboot/jms/listener/SqsListener.java
@@ -0,0 +1,17 @@
+package biz.belcorp.spring.springboot.jms.listener;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+
+@Component
+@Log4j2
+public class SqsListener {
+    @JmsListener(destination = "test1")
+    public void receive(String message) throws JMSException {
+        log.info("Received message {}", message);
+        System.out.println(message);
+    }
+}
---
+++ src/test/java/biz/belcorp/spring/springboot/jms/SpringBootJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.belcorp.spring.springboot.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
